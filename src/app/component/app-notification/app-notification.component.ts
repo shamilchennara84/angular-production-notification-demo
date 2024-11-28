daa426d8283ce5a4b825f0523863672a1e09d56c
Shamil P P <shamil.pp@neutrinos.co>
2024-11-28 12:55:43 +0530

    version reseted to 1.0.0 and service login updated

--- src/app/component/app-notification/app-notification.component.ts
+++ src/app/component/app-notification/app-notification.component.ts
@@ -8,36 +8,41 @@ import { interval, Subscription } from 'rxjs';
   styleUrls: ['./app-notification.component.scss']
 })
 export class AppNotificationComponent implements OnInit, OnDestroy {
-  showNotification: boolean = false;  // Make sure this property is correctly typed
+  showNotification: boolean = false;
   notificationMessage: string = 'A new build is available. Please refresh the page to update.';
   private subscription: Subscription | undefined;
 
   constructor(private versionService: VersionCheckService) {}
 
   ngOnInit(): void {
-    // Start the interval to check for updates every minute
     this.subscription = interval(60000).subscribe(() => {
       this.checkForUpdates();
     });
   }
 
-  
-    checkForUpdates(): void {
-      this.versionService.checkForUpdates().subscribe((response) => {
-        const newVersion = response.version;
-        if (this.versionService.isUpdateAvailable(newVersion)) {
-          this.showNotification = true;
-        }
-      });
-    }
+  checkForUpdates(): void {
+    this.versionService.checkForUpdates().subscribe((response) => {
+      const newVersion = response.version;
+      if (this.versionService.isUpdateAvailable(newVersion)) {
+        this.showNotification = true;
+      }
+    });
+  }
+
   ngOnDestroy(): void {
-    // Unsubscribe to prevent memory leaks
+   
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
   }
 
   refreshPage(): void {
+    
+    sessionStorage.clear();
     window.location.reload();
+  }
+
+  onUserAcknowledge(): void {
+    this.refreshPage();
   }
 }
--- src/services/version-check.service.ts
+++ src/services/version-check.service.ts
@@ -1,21 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';  // to tap into the observable response
 
 @Injectable({
   providedIn: 'root'
 })
 export class VersionCheckService {
-  private currentVersion = '1.0.0'; 
   private versionFile = 'assets/version.json';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.initializeCurrentVersion();
+  }
+
+  private initializeCurrentVersion(): void {
+    if (!sessionStorage.getItem('currentVersion')) {
+      this.http.get<any>(this.versionFile).pipe(
+        tap(response => {
+          const currentVersion = response.version || '1.0.0';  
+          sessionStorage.setItem('currentVersion', currentVersion);
+        })
+      ).subscribe();
+    }
+  }
 
   checkForUpdates(): Observable<any> {
     return this.http.get<any>(this.versionFile);
   }
 
   isUpdateAvailable(newVersion: string): boolean {
-    return newVersion !== this.currentVersion;
+    const currentVersion = sessionStorage.getItem('currentVersion') ; 
+    return newVersion !== currentVersion;
+  }
+
+  updateVersion(newVersion: string): void {
+    sessionStorage.setItem('currentVersion', newVersion);
   }
 }
